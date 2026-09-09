10ccb4cccc94e44fc7201637cc2fa1bc192a7db1
Tnhan1611 <ffrach123@gamil.com>
2026-09-09 14:16:26 +0700

    Prepare Flask app for Render

--- web/app.py
+++ web/app.py
@@ -133,10 +133,11 @@ def predict():
 # RUN SERVER
 # ==============================
 
-if __name__ == "__main__":
+import os
 
+if __name__ == "__main__":
     app.run(
-        debug=True,
-        host="127.0.0.1",
-        port=5000
+        debug=False,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
     )
